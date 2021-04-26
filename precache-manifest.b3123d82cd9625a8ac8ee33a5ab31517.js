b2027d718dee314d64967d217c0e26f2d0d39b91
xWafl <35458851+xWafl@users.noreply.github.com>
2021-04-26 13:28:07 -0400

    Add files via upload

---
+++ precache-manifest.b3123d82cd9625a8ac8ee33a5ab31517.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769a9478f06fb4d2d462",
+    "url": "/css/app.1fba6623.css"
+  },
+  {
+    "revision": "3a84d00d92e1f0cc66eb",
+    "url": "/css/chunk-vendors.25d7cad0.css"
+  },
+  {
+    "revision": "2e935203e7200edebf345ee19a80f435",
+    "url": "/fonts/RobotoSlab-Regular.2e935203.ttf"
+  },
+  {
+    "revision": "390ea8a9d2561b99fabd70554a1b46d2",
+    "url": "/img/BG.390ea8a9.png"
+  },
+  {
+    "revision": "5a4cdfc27f8c3245428a8374e0b2b2c2",
+    "url": "/img/CC Extras.5a4cdfc2.png"
+  },
+  {
+    "revision": "b2aad842bd722ce39b8595a303b39d57",
+    "url": "/img/PGW.b2aad842.png"
+  },
+  {
+    "revision": "f01f6b6b99c7e832839ef2a894341983",
+    "url": "/img/bananya.f01f6b6b.png"
+  },
+  {
+    "revision": "becc63b79f8e64e6c084b2fd1b061fd8",
+    "url": "/img/cc-logo-bw.becc63b7.png"
+  },
+  {
+    "revision": "ecea1bb7f9f8006951b444ae6df7bfc3",
+    "url": "/img/indian.ecea1bb7.png"
+  },
+  {
+    "revision": "8347bead1515fb6cb76ec53564769ba2",
+    "url": "/img/luke.8347bead.png"
+  },
+  {
+    "revision": "e168aabdcd3d770d8b53c179508714c2",
+    "url": "/img/maddy.e168aabd.png"
+  },
+  {
+    "revision": "8b0162f0a545e6620f48804428214c57",
+    "url": "/img/mao.8b0162f0.png"
+  },
+  {
+    "revision": "0d1e51e4b5515fa084f19c2298ef46e6",
+    "url": "/img/toslaw.0d1e51e4.png"
+  },
+  {
+    "revision": "d1e797eee443155d9c1a496c0b88d664",
+    "url": "/img/vyc.d1e797ee.png"
+  },
+  {
+    "revision": "1e721d7c35b3894d916d24a7eeb2efea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f78b28b81e3b6012e3e8",
+    "url": "/js/about.73ff255e.js"
+  },
+  {
+    "revision": "769a9478f06fb4d2d462",
+    "url": "/js/app.f0f2945d.js"
+  },
+  {
+    "revision": "3a84d00d92e1f0cc66eb",
+    "url": "/js/chunk-vendors.9d16ca58.js"
+  },
+  {
+    "revision": "0e2681ac601ef3f8df9df2c38b762a78",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
